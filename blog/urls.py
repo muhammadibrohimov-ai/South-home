0584447f2315300738f39d3b469b9067f7c01559
Muhammadjon Ibrohimov <ibrokhimovmiu@gmail.com>
2025-10-20 10:22:59 +0500

    changed

--- blog/urls.py
+++ blog/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import blog_view, single_blog_view
+from .views import blog_view, single_blog_view, blog_category_view
 
 urlpatterns = [
     path('', blog_view),
+    path('category/<int:id>', blog_category_view),
     path('<int:id>', single_blog_view),     
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,10 +1,28 @@
 from django.shortcuts import render
 from .models import Blog, BlogCategory
+from main.models import Home
+from django.shortcuts import get_object_or_404
 
 # Create your views here.
 
+
 def blog_view(request):
-    return render(request=request, template_name='blog/blog.html', context={'blogs':Blog.objects.all(), 'categories':BlogCategory.objects.all()})
+    return render(request=request, template_name='blog/blog.html', context={'blogs':Blog.objects.all(), 'categories':BlogCategory.objects.all(), "homes":Home.objects.all()})
+
+
+def blog_category_view(request, id):
+    
+    try:
+        category = BlogCategory.objects.get(id = id)
+        blogs = Blog.objects.filter(category = category)
+    
+    except:
+        blogs = False
+        
+    return render(request=request, template_name='blog/blog.html', context={'blogs':blogs, 'categories':BlogCategory.objects.all(), "homes":Home.objects.all()})
+
 
 def single_blog_view(request, id):
-    return render(request=request, template_name='blog/single-blog.html', context={'blogs':Blog.objects.get(id=id), 'categories':BlogCategory.objects.all()})
+    
+    
+    return render(request=request, template_name='blog/single-blog.html', context={'blog':Blog.objects.get(id=id), 'categories':BlogCategory.objects.all()})
